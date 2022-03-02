7d90afd707ad63959629ceb8ba6ca5475de50838
letscombine <qkrtkdwns3410@naver.com>
2022-03-02 19:05:13 +0900

    Initial commit

---
+++ app/src/main/java/com/example/contactlistapplication/ContactDetailActivity.java
@@ -0,0 +1,12 @@
+package com.example.contactlistapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactDetailActivity extends AppCompatActivity {
+	  
+	  @Override
+	  protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+	  }
+}
---
+++ app/src/main/java/com/example/contactlistapplication/CreateNewActivity.java
@@ -0,0 +1,15 @@
+package com.example.contactlistapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateNewActivity extends AppCompatActivity {
+	  
+	  @Override
+	  protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			
+			
+			
+	  }
+}
