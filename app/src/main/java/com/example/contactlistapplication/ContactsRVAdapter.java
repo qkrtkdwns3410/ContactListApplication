56e2ff7e944fc000a1f839444364b6a4c81e2d2d
letscombine <qkrtkdwns3410@naver.com>
2022-03-03 11:58:22 +0900

    changes:2022/03/03

--- app/src/main/java/com/example/contactlistapplication/ContactsRVAdapter.java
+++ app/src/main/java/com/example/contactlistapplication/ContactsRVAdapter.java
@@ -1,12 +1,18 @@
 package com.example.contactlistapplication;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.TreeMap;
 
+import com.amulyakhare.textdrawable.TextDrawable;
 import com.example.contactlistapplication.DTO.ContactsModal;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
@@ -51,13 +57,34 @@ public class ContactsRVAdapter extends RecyclerView.Adapter<ContactsRVAdapter.Vi
 			//뷰홀더에 텍스트 설정
 			holder.contactTextView.setText(modal.getContactName());
 		 
-			ColorGenerater generater = new ColorGenerater.MATERIAL;
+			Random rnd = new Random();
+			int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+		 
+			TextDrawable drawable2 = TextDrawable.builder()
+				.beginConfig()
+				.width(100)
+				.height(100)
+				.endConfig()
+				.buildRound(modal.getUserName().substring(0, 1), color);
+		  
+			//TextDrawable 설정을 해당 이미지뷰에 설정합니다.
+			holder.contactImageView.setImageDrawable(drawable2);
 			
+			holder.itemView.setOnClickListener(new View.OnClickListener() {
+				  @Override
+				  public void onClick(View view) {
+						Intent intent = new Intent(context, ContactDetailActivity.class);
+						intent.putExtra("name", modal.getUserName());
+						intent.putExtra("contact", modal.getContactName());
+					 
+						context.startActivity(intent);
+				  }
+			});
 	  }
 	  
 	  @Override
 	  public int getItemCount() {
-			return 0;
+			return contactsModalArrayList.size();
 	  }
 	  
 	  public class ViewHolder extends RecyclerView.ViewHolder {
@@ -67,7 +94,8 @@ public class ContactsRVAdapter extends RecyclerView.Adapter<ContactsRVAdapter.Vi
 			public ViewHolder(@NonNull View itemView) {
 				  super(itemView);
 				  
-				  contactImageView =
+				  contactImageView = itemView.findViewById(R.id.idIVContact);
+				  contactTextView = itemView.findViewById(R.id.idTVContactName);
 			}
 	  }
 	  
